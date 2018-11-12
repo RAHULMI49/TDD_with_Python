e5d6cf609823e741f0887d7eada82858643b53bd
rahulmi182 <rahulmittal182@gmail.com>
2018-11-12 19:08:33 +0530

    functional test now checks whether we can input a item in list or not

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVistorTest(unittest.TestCase):
 
@@ -10,8 +12,25 @@ class NewVistorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
+		
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do', self.browser.title)
+
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to - do list')
+		inputbox.send_keys('Buy a peacock')
+
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue( any(row.text == '1 : Buy a peacock' for row in rows) )
+
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
